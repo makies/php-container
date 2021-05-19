789dcffabad2000e390846d18c2d0d68f03d9110
Maki Fujiwara <m_fujiwara@yumemi.co.jp>
2021-05-19 09:05:20 +0900

    src移動

--- app1/src/routes/web.php
+++ app1/src/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    phpinfo();
     return view('welcome');
+});
+
+Route::get('/info', function () {
+    phpinfo();
 });
